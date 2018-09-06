967b42898da96ef7fe76b8694a635bf97fff1079
saturator22 <f.szczeski@gmail.com>
2018-09-06 14:50:04 +0200

    Change method name to loadCSV

--- bill-generator/src/main/java/com/codecool/DataParser.java
+++ bill-generator/src/main/java/com/codecool/DataParser.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class DataParser {
 
     public List<Product> parse() {
-        List<String[]> data = DataLoader.load();
+        List<String[]> data = DataLoader.loadCSV();
         List<Product> productsList = new ArrayList<>();
 
         for (String[] subData: data) {
